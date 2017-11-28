f89aa9caf385ffaed3d5e35a80aa647301edb122
Unknown <nichapat18102541@gmail.com>
2017-11-28 11:34:47 +0700

    ทำข้อที่ 3

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -6,11 +6,18 @@ int main() {
 
     const float pi = 3.14315926536;
     float r;
+    float area;
+    float e = 2;
+    float q = 15;
 
     cout << "Please enter radius : ";
     cin >> r;
+    area = pi * r * r;
+    cout << "Area of circle : "<< area << endl;
 
-    cout << "Area of circle : "<< pi * r * r << endl;
+    cout << "Circumference : "<< e * pi * r<< endl;
+
+    cout << "Cylinder height 15 : "<< pi * (r*r) * q<< endl;
 
   return 0;
 }
