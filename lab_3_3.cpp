602fd572d1e5cba0d7c084e9de52e13da30e9cbb
Unknown <nichapat18102541@gmail.com>
2017-11-28 11:14:57 +0700

    ทำต่อจากเมื่อวาน ข้อที่ 2 (เสร็จแล้ว)

--- lab_3_3.cpp
+++ lab_3_3.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
-  
+
+    const float pi = 3.14315926536;
+    float r;
+
+    cout << "Please enter radius : ";
+    cin >> r;
+
+    cout << "Area of circle : "<< pi * r * r << endl;
+
   return 0;
 }
